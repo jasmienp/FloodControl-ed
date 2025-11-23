adc77c6a8445998aaa7a87d965bf153da8986d62
Jasmien <jasmien.pajiji9@gmail.com>
2025-11-23 14:16:29 +0800

    timer

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -3,3 +3,25 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://scenes/level_select_menu.tscn")
 	LevelCore.lvl2_completed = true
+
+func _ready():
+	reset_timer()
+	$Timer.start()
+	_on_timer_timeout() 
+	
+var seconds = 0
+var minutes = 0
+var Dseconds = 30
+var Dminutes = 2
+
+func _on_timer_timeout() -> void:
+	if seconds == 0:
+		if minutes > 0:
+			minutes -= 1
+			seconds = 60		
+	seconds -= 1
+	$TimerLabel.text = str(minutes) + ":" + str(seconds)
+
+func reset_timer():
+	seconds = Dseconds
+	minutes = Dminutes
