e83371d80f9002f29262a097f0a85415db8a38b6
maaliyasmien-source <maaliyasmien@gmail.com>
2025-11-25 17:58:26 +0800

    connected epilogue to existing scenes

--- scripts/epilogue.gd
+++ scripts/epilogue.gd
@@ -14,4 +14,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://letter.tscn")
+	get_tree().change_scene_to_file("res://scenes/letter.tscn")
--- scripts/lets_go.gd
+++ scripts/lets_go.gd
@@ -1,15 +1,12 @@
 extends Node2D
 
+@onready var lg = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	lg.play("lets_go")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_exit_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/lets_go.tscn")
